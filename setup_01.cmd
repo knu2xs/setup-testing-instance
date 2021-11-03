2e4ad8b9dd2cd35f7c844057529e6aaa583ce8d1
Joel McCune <knu2xs@gmail.com>
2021-11-03 13:25:10 -0700

    Add code for installing chocolatey from docs

--- setup_01.cmd
+++ setup_01.cmd
@@ -1,11 +1,13 @@
-RUN @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" 
-SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+SET DIR=%~dp0%
 
-RUN refreshenv
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://community.chocolatey.org/install.ps1','%DIR%install.ps1'))"
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%install.ps1' %*"
 
-RUN choco install 7zip vscode git anaconda3 -y
+REFRESHENV
 
-RUN refreshenv
+choco install 7zip vscode git anaconda3 -y
+
+REFRESHENV
 
 IF NOT EXIST "C:/setup" MKDIR "C:/setup"
 IF NOT EXIST "C:/setup/pro" MKDIR "C:/setup/pro"
@@ -17,7 +19,7 @@ XCOPY "\\redarchive4\business_analyst_public\US_2021\Builds\2.8.0.8\SDLICs\BA_20
 START /WAIT msiexec.exe /i "C:\setup\data\setup.msi" /qb AgreeToLicense=Yes
 START /WAIT "C:\Program Files\ArcGIS\Pro\bin\DataLicInstall.exe" "C:\setup\data\BA_2021_US_NAT_Data_Bundle_w_MPI.sdlic"
 
-RUN RMDIR /q /s C:\setup\data
+RMDIR /q /s C:\setup\data
 
-RUN explorer \\metro
-RUN explorer \\archive2\ProSetups
+EXPLORER \\metro
+EXPLORER \\archive2\ProSetups
