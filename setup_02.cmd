064b290263de9bfae584b3f5c1c016a970d75c11
Joel McCune <knu2xs@gmail.com>
2021-11-03 14:30:52 -0700

    Update setup_02.cmd

--- setup_02.cmd
+++ setup_02.cmd
@@ -2,8 +2,6 @@ START /WAIT msiexec.exe /i "C:\setup\pro\ArcGISPro.msi" /qb ALLUSERS=1 SOFTWARE_
 
 START /WAIT "%PROGRAMFILES%\ArcGIS\Pro\bin\SoftwareAuthorizationPro.exe" /LIF C:/setup/Pro_AdvancedSU.prvc /s
 
-RMDIR /q /s C:\setup\pro
-
 RUN conda create -n arcgis --clone "%PROGRAMFILES%\ArcGIS\Pro\bin\Python\envs\arcgispro-py3"
 
 RUN "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\proswap.bat" -p "C:\tools\Anaconda3\envs\arcgis"
