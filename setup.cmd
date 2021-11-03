a1e7907a05ff15c18451b610bb8feadf5db02780
Joel McCune <knu2xs@gmail.com>
2021-11-03 15:03:38 -0700

    Update and rename setup_01.cmd to setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -18,6 +18,16 @@ IF %ERRORLEVEL% EQU 0 (
    EXIT /B 1
 )
 
+IF NOT EXIST "C:/setup" MKDIR "C:/setup"
+IF NOT EXIST "C:/setup/pro" MKDIR "C:/setup/pro"
+IF NOT EXIST "C:/setup/data" MKDIR "C:/setup/data"
+
+ECHO. Please copy ArcGIS Pro installation resources to C:/setup/pro
+explorer \\metro
+explorer \\archive2\ProSetups
+explorer C:\setup\pro
+PAUSE
+
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco feature enable -n=allowGlobalConfirmation
@@ -25,17 +35,22 @@ choco feature enable -n=allowGlobalConfirmation
 choco install 7zip 
 choco install vscode 
 choco install git 
-choco install anaconda3
+choco install anaconda3 --params '"/AddToPath"'
 
-IF NOT EXIST "C:/setup" MKDIR "C:/setup"
-IF NOT EXIST "C:/setup/pro" MKDIR "C:/setup/pro"
-IF NOT EXIST "C:/setup/data" MKDIR "C:/setup/data"
+REFRESHENV
 
 XCOPY "\\redarchive4\business_analyst_public\US_2021\Builds\2.8.0.8\ESRI_BA_2021_US_Data_Update" "C:\setup\data\" /E /H
 
 START /WAIT msiexec.exe /i "C:\setup\data\setup.msi" /qb AgreeToLicense=Yes
 
-RMDIR /q /s C:\setup\data
+START /WAIT msiexec.exe /i "C:\setup\pro\ArcGISPro.msi" /qb ALLUSERS=1 SOFTWARE_CLASS=Professional AUTHORIZATION_TYPE=SINGLE_USE ACCEPTEULA=yes
 
-explorer \\metro
-explorer \\archive2\ProSetups
+XCOPY "\\Esri.com\software\Esri\Released\Authorization_Files\ArcGISPro2.9\Single Use\Pro_AdvancedSU.prvc" "C:\setup\pro"
+"C:\Program Files\ArcGIS\Pro\bin\SoftwareAuthorizationPro.exe" -LIF "C:\setup\pro\Pro_AdvancedSU.prvc"
+
+XCOPY "\\redarchive4\business_analyst_public\US_2021\Builds\2.8.0.8\SDLICs\BA_2021_US_NAT_Data_Bundle_w_MPI.sdlic" "C:\setup\pro"
+"C:\Program Files\ArcGIS\Pro\bin\DataLicInstall.exe" "C:\setup\pro\BA_2021_US_NAT_Data_Bundle_w_MPI.sdlic"
+
+conda create -n arcgis --clone "%PROGRAMFILES%\ArcGIS\Pro\bin\Python\envs\arcgispro-py3"
+
+"C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\proswap.bat" -p "C:\tools\Anaconda3\envs\arcgis"
