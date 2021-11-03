06974f2fe4eb52e0ae65059089bdc5b47b0b5b90
Joel McCune <knu2xs@gmail.com>
2021-11-03 14:19:41 -0700

    Remove data license since Pro not yet installed

--- setup_01.cmd
+++ setup_01.cmd
@@ -32,12 +32,10 @@ IF NOT EXIST "C:/setup/pro" MKDIR "C:/setup/pro"
 IF NOT EXIST "C:/setup/data" MKDIR "C:/setup/data"
 
 XCOPY "\\redarchive4\business_analyst_public\US_2021\Builds\2.8.0.8\ESRI_BA_2021_US_Data_Update" "C:\setup\data\" /E /H
-XCOPY "\\redarchive4\business_analyst_public\US_2021\Builds\2.8.0.8\SDLICs\BA_2021_US_NAT_Data_Bundle_w_MPI.sdlic" "C:\setup\data"
 
 START /WAIT msiexec.exe /i "C:\setup\data\setup.msi" /qb AgreeToLicense=Yes
-START /WAIT "C:\Program Files\ArcGIS\Pro\bin\DataLicInstall.exe" "C:\setup\data\BA_2021_US_NAT_Data_Bundle_w_MPI.sdlic"
 
 RMDIR /q /s C:\setup\data
 
-EXPLORER \\metro
-EXPLORER \\archive2\ProSetups
+explorer \\metro
+explorer \\archive2\ProSetups
