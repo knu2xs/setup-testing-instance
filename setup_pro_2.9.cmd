8266c220df213b70dc845a67fae91a6b77fa8e5f
Joel McCune <knu2xs@gmail.com>
2021-11-04 21:09:20 -0700

    Update setup_pro_2.9.cmd

--- setup_pro_2.9.cmd
+++ setup_pro_2.9.cmd
@@ -20,7 +20,6 @@ IF %ERRORLEVEL% EQU 0 (
 
 IF NOT EXIST "C:/setup" MKDIR "C:/setup"
 IF NOT EXIST "C:/setup/pro" MKDIR "C:/setup/pro"
-IF NOT EXIST "C:/setup/data" MKDIR "C:/setup/data"
 
 ECHO. Please copy ArcGIS Pro installation resources to C:/setup/pro
 explorer \\metro
@@ -37,18 +36,14 @@ choco install vscode
 choco install git 
 choco install anaconda3 --params '"/AddToPath"'
 
-REFRESHENV
-
 START /WAIT msiexec.exe /i "C:\setup\pro\ArcGISPro.msi" /qb ALLUSERS=1 SOFTWARE_CLASS=Professional AUTHORIZATION_TYPE=SINGLE_USE ACCEPTEULA=yes
 
-XCOPY "\\Esri.com\software\Esri\Released\Authorization_Files\ArcGISPro2.9\Single Use\Pro_AdvancedSU.prvc" "C:\setup\pro"
-"C:\Program Files\ArcGIS\Pro\bin\SoftwareAuthorizationPro.exe" /LIF "C:\setup\pro\Pro_AdvancedSU.prvc" /s
+"C:\Program Files\ArcGIS\Pro\bin\SoftwareAuthorizationPro.exe" /LIF "\\Esri.com\software\Esri\Released\Authorization_Files\ArcGISPro2.9\Single Use\Pro_AdvancedSU.prvc" /s
 
-XCOPY "\\redarchive4\business_analyst_public\US_2021\Builds\2.8.0.8\ESRI_BA_2021_US_Data_Update" "C:\setup\data\" /e /h
-START /WAIT msiexec.exe /i "C:\setup\data\setup.msi" /qb AgreeToLicense=Yes
+REG import "\\redarchive4\business_analyst_public\US_2021\US_2021_Install_Workaround\US_2021_Installation_Workaround.reg"
+START /WAIT msiexec.exe /i "\\redarchive4\business_analyst_public\US_2021\Builds\2.8.0.8\ESRI_BA_2021_US_Data_Update\setup.msi" /qb AgreeToLicense=Yes
 
-XCOPY "\\redarchive4\business_analyst_public\US_2021\Builds\2.8.0.8\SDLICs\BA_2021_US_NAT_Data_Bundle_w_MPI.sdlic" "C:\setup\pro"
-"C:\Program Files\ArcGIS\Pro\bin\DataLicInstall.exe" "C:\setup\pro\BA_2021_US_NAT_Data_Bundle_w_MPI.sdlic"
+"C:\Program Files\ArcGIS\Pro\bin\DataLicInstall.exe" "\\redarchive4\business_analyst_public\US_2021\Builds\2.8.0.8\SDLICs\BA_2021_US_NAT_Data_Bundle_w_MPI.sdlic"
 
 conda create -n arcgis --clone "%PROGRAMFILES%\ArcGIS\Pro\bin\Python\envs\arcgispro-py3"
 
