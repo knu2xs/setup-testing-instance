75ae29eff411be204ba1980f4487428df8eccc99
Joel McCune <knu2xs@gmail.com>
2021-12-13 15:57:55 -0800

    Update setup_pro_2.9.cmd

--- setup_pro_2.9.cmd
+++ setup_pro_2.9.cmd
@@ -40,8 +40,6 @@ CALL refreshenv
 
 START /WAIT msiexec.exe /i "C:\setup\pro\ArcGISPro.msi" /qb ALLUSERS=1 SOFTWARE_CLASS=Professional AUTHORIZATION_TYPE=SINGLE_USE ACCEPTEULA=yes
 
-"C:\Program Files\ArcGIS\Pro\bin\SoftwareAuthorizationPro.exe" /LIF "\\Esri.com\software\Esri\Released\Authorization_Files\ArcGISPro2.9\Single Use\Pro_AdvancedSU.prvc" /s
-
 REG import "\\redarchive4\business_analyst_public\US_2021\US_2021_Install_Workaround\US_2021_Installation_Workaround.reg"
 START /WAIT msiexec.exe /i "\\redarchive4\business_analyst_public\US_2021\Builds\2.8.0.8\ESRI_BA_2021_US_Data_Update\setup.msi" /qb AgreeToLicense=Yes
 
