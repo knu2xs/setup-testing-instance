f77da11596e6fcd9298e47825d8af8e94a359f57
Joel McCune <knu2xs@gmail.com>
2022-02-03 06:33:37 -0800

    Get correct version of DotNet installed

--- setup_pro_3.0.cmd
+++ setup_pro_3.0.cmd
@@ -35,7 +35,7 @@ choco install 7zip
 choco install vscode 
 choco install git 
 choco install anaconda3 --params '"/AddToPath"'
-choco install dotnet-5.0-desktopruntime
+choco install dotnet5-desktop-runtime --version=5.0.0
 
 CALL refreshenv
 
