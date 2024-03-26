8fa1ba3acb8aa115742d2d75a033b11e25c04d22
Joel McCune <knu2xs@gmail.com>
2024-03-26 12:17:25 -0700

    Update license_single_use.cmd

--- license_single_use.cmd
+++ license_single_use.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files\ArcGIS\Pro\bin\SoftwareAuthorizationPro.exe" /LIF "\\Esri.com\software\Esri\Released\Authorization_Files\ArcGISPro2.9\Single Use\Pro_AdvancedSU.prvc" /s
+"C:\Program Files\ArcGIS\Pro\bin\SoftwareAuthorizationPro.exe" /LIF "\\Esri.com\software\Esri\Released\Authorization_Files\ArcGISPro3.2\Single Use\Pro_AdvancedSU.prvc" /s
