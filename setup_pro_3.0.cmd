66f5281b076e25ead82cfeb94bab0490dc3424fa
Joel McCune <knu2xs@gmail.com>
2024-06-03 14:39:31 -0700

    Update setup_pro_3.0.cmd

--- setup_pro_3.0.cmd
+++ setup_pro_3.0.cmd
@@ -3,19 +3,19 @@ ECHO OFF
 NET SESSION >nul 2>&1
 
 IF %ERRORLEVEL% EQU 0 (
-   ECHO.
+   ECHO.
 
 ) ELSE (
 
-   ECHO.-------------------------------------------------------------
-   ECHO ERROR: YOU ARE NOT RUNNING THIS WITH ADMINISTRATOR PRIVILEGES.
-   ECHO. -------------------------------------------------------------
-   ECHO. If you're seeing this, it means you don't have admin privileges!
-   ECHO.
-   ECHO. You will need to restart this script with Administrator privileges by right-clicking and select "Run As Administrator"
-   PAUSE
+   ECHO.-------------------------------------------------------------
+   ECHO ERROR: YOU ARE NOT RUNNING THIS WITH ADMINISTRATOR PRIVILEGES.
+   ECHO. -------------------------------------------------------------
+   ECHO. If you're seeing this, it means you don't have admin privileges!
+   ECHO.
+   ECHO. You will need to restart this script with Administrator privileges by right-clicking and select "Run As Administrator"
+   PAUSE
 
-   EXIT /B 1
+   EXIT /B 1
 )
 
 IF NOT EXIST "C:/setup" MKDIR "C:/setup"
@@ -34,18 +34,22 @@ choco feature enable -n=allowGlobalConfirmation
 choco install 7zip 
 choco install vscode 
 choco install git 
-choco install anaconda3 --params '"/AddToPath"'
+
+choco install anaconda3
+setx /M path "%path%;C:\tools\Anaconda3;C:\tools\Anaconda3\condabin"
+
 choco install dotnet-desktopruntime
 
 CALL refreshenv
 
 START /WAIT msiexec.exe /i "C:\setup\pro\ArcGISPro.msi" /qb ALLUSERS=1 SOFTWARE_CLASS=Professional AUTHORIZATION_TYPE=SINGLE_USE ACCEPTEULA=yes LOCK_AUTH_SETTINGS=FALSE
 
-REG import "\\redarchive4\business_analyst_public\US_2021\US_2021_Install_Workaround\US_2021_Installation_Workaround.reg"
-START /WAIT msiexec.exe /i "\\redarchive4\business_analyst_public\US_2021\Builds\2.8.0.8\ESRI_BA_2021_US_Data_Update\setup.msi" /qb AgreeToLicense=Yes
+IF NOT EXIST "D:/data/ba_data" MKDIR "D:/data/ba_data"
 
-CALL "C:\Program Files\ArcGIS\Pro\bin\DataLicInstall.exe" "\\redarchive4\business_analyst_public\US_2021\Builds\2.8.0.8\SDLICs\BA_2021_US_NAT_Data_Bundle_w_MPI.sdlic"
+CALL robocopy "\\redarchive4\business_analyst_public\US_2024\Builds\3.3.1.3\ESRI_BA_2024_US_Data_Update" "D:/data/ba_data/usa_2024" /mir /mt /z
+
+CALL "%PROGRAMFILES%\ArcGIS\Pro\bin\DataLicInstall.exe" "\\redarchive4\business_analyst_public\US_2024\Builds\3.3.1.3\SDLICs\Internal-BA-USA-2024-NAT-MPI-05_27_25.sdlic"
 
 conda create -n arcgis --clone "%PROGRAMFILES%\ArcGIS\Pro\bin\Python\envs\arcgispro-py3"
 
-CALL "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\proenv.bat" -p "C:\tools\Anaconda3\envs\arcgis"
+CALL "%PROGRAMFILES%\ArcGIS\Pro\bin\Python\Scripts\proenv.bat" -p "C:\tools\Anaconda3\envs\arcgis"
