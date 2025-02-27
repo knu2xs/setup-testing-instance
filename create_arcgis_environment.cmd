5fb2750b8876bb4a279eea73fb4278f8a6f8affe
Joel McCune <knu2xs@gmail.com>
2025-02-26 21:11:43 -0800

    Create create_arcgis_environment.cmd

---
+++ create_arcgis_environment.cmd
@@ -0,0 +1,3 @@
+conda create -n arcgis --clone "%PROGRAMFILES%\ArcGIS\Pro\bin\Python\envs\arcgispro-py3"
+
+CALL "%PROGRAMFILES%\ArcGIS\Pro\bin\Python\Scripts\proenv.bat" -p "C:\tools\Anaconda3\envs\arcgis"
