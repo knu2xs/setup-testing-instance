6675f760f3e165f6ac05ab4aa0bd4f732e847e02
Joel McCune <knu2xs@gmail.com>
2025-06-23 13:43:20 -0700

    Update license_single_use.cmd
    udpate license to 3.5

--- license_single_use.cmd
+++ license_single_use.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files\ArcGIS\Pro\bin\SoftwareAuthorizationPro.exe" /LIF "\\Esri.com\software\Esri\Released\Authorization_Files\ArcGISPro3.4\Single Use\Pro_AdvancedSU.prvc" /s
+"C:\Program Files\ArcGIS\Pro\bin\SoftwareAuthorizationPro.exe" /LIF "\\Esri.com\software\Esri\Released\Authorization_Files\ArcGISPro3.5\Single Use\Pro_AdvancedSU.prvc" /s
